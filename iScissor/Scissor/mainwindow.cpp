7f99e3da07413feedf77772f92c838eaa2cbe489
Yuan CHEN <constantchen525@gmail.com>
2018-02-18 09:35:58 +0800

    initialization crashed

--- iScissor/Scissor/mainwindow.cpp
+++ iScissor/Scissor/mainwindow.cpp
@@ -309,6 +309,7 @@ void MainWindow::on_actionOpen_triggered()
     head_node = NULL;
     pathTree = new QImage(drawPathTree());
     computeCostFunc();
+    cout << "cost finished" << endl;
 
     ui->label->setPixmap(QPixmap::fromImage(Q_img));
 
